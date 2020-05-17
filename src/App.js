3b1384239e66806eee1e37160205e162bad5a71c
mjtoolbox <michael.jo@gmail.com>
2020-05-17 10:55:03 -0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light static-top'>
+        <div className='container'>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item active'></li>
+            <li className='nav-item'></li>
+          </ul>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route exact path='/' />
+        <Route exact path='/login' />
+      </Switch>
+    </Router>
   );
 }
 
